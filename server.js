f4fde29efd4ae5099b4a56c79a4de8b73ed23006
fuferro-lab <fuferro@gmail.com>
2026-03-23 17:21:18 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
+// serve nmp install pg e npm install ejs
+import { Client } from 'pg';
+import express from 'express';
 const app = express();
+//const express = require('express');
 const port = 3000;
+const client = new Client({
+  //connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://neondb_owner:npg_S8wDvg1lcqfM@ep-damp-voice-agmvqylt-pooler.c-2.eu-central-1.aws.neon.tech/ese01?sslmode=require&channel_binding=require",
+  ssl: true, // Necessario per Neon
+});
+await client.connect();
+const ris = await client.query('SELECT * FROM utenti');
 
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   // Dati dinamici (es. da database)
-  const data = { title: 'Pagina SSR', content: 'Contenuto della pagina sul server' };
+  const d = { title: 'Pagina SSR', content: 'Contenuto della pagina sul server' };
 
-  // Rendering lato server: il server compila l'HTML
-  const html = `
-    <html>
-      <head><title>${data.title}</title></head>
-      <body>
-        <h1>${data.title}</h1>
-        <p>${data.content}</p>
-        <p>Ciao Mondo Render!!</p>
-      </body>
-    </html>
-  `;
-  res.send(html); // Invia HTML completo al client
+  res.render('risposta', { dat: d, dati: ris.rows });  
 });
 
 app.listen(port, () => {
