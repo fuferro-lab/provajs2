bf41eef4590369df227353a32adb1ef77b294cb0
fuferro-lab <fuferro@gmail.com>
2026-03-25 13:52:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 const client = new Client({
   //connectionString: process.env.DATABASE_URL,
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://neondb_owner:npg_S8wDvg1lcqfM@ep-damp-voice-agmvqylt-pooler.c-2.eu-central-1.aws.neon.tech/ese01?sslmode=require&channel_binding=require",
   ssl: true, // Necessario per Neon
 });
 await client.connect();
