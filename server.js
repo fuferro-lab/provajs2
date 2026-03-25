a968356c4639ab0e71e50a3fbbfe90f4ed2f7539
fuferro-lab <fuferro@gmail.com>
2026-03-25 13:55:33 +0100

    Update database connection string and content message

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 //const express = require('express');
 const port = 3000;
 const client = new Client({
-  //connectionString: process.env.DATABASE_URL,
-  connectionString: "postgresql://neondb_owner:npg_S8wDvg1lcqfM@ep-damp-voice-agmvqylt-pooler.c-2.eu-central-1.aws.neon.tech/ese01?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL,
+  //connectionString: "postgresql://neondb_owner:npg_S8wDvg1lcqfM@ep-damp-voice-agmvqylt-pooler.c-2.eu-central-1.aws.neon.tech/ese01?sslmode=require&channel_binding=require",
   ssl: true, // Necessario per Neon
 });
 await client.connect();
@@ -15,7 +15,7 @@ const ris = await client.query('SELECT * FROM utenti');
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   // Dati dinamici (es. da database)
-  const d = { title: 'Pagina SSR', content: 'Contenuto della pagina sul server' };
+  const d = { title: 'Pagina SSR', content: 'Pagina sul server' };
 
   res.render('risposta', { dat: d, dati: ris.rows });  
 });
